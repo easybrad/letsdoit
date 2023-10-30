741092a344a4fc967ef294f8cfac07fa12e6e087
brad-mac <brad.ma.mail@gmail.com>
2023-10-31 00:18:15 +0800

    屏蔽 'React' must be in scope when using JSX 报错

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-const App = () => (
-  <div className="logs">
-    {/* 日志项容器 */}
-    <div className="item">
-      <div className="date">
-        <div className="month">四月</div>
-        <div className="day">10</div>
-      </div>
-      <div className="content">
-        <h2 className="desc">学习 React</h2>
-        <div className="time">40分钟</div>
-      </div>
-    </div>
-    <div className="item">
-      <div className="date">
-        <div className="month">四月</div>
-        <div className="day">10</div>
-      </div>
-      <div className="content">
-        <h2 className="desc">学习 React</h2>
-        <div className="time">40分钟</div>
-      </div>
-    </div>
-  </div>
-);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
